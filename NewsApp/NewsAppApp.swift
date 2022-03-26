a6f6016c01b4e6955d2db6622388746df3cd7a66
Tiso <tisobyn@gmail.com>
2022-03-26 19:47:13 +0600

    Initial commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Yermek Sabyrzhan on 26.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
